bb564297683ffeac3d5d3d643f614813823fff3c
SberbankHomeWork <alexeytroitskii@gmail.com>
2019-02-28 20:26:16 +0300

    fourth version

--- app/src/main/java/com/alextroy/android/hw3_service/MyService.java
+++ app/src/main/java/com/alextroy/android/hw3_service/MyService.java
@@ -4,21 +4,24 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.widget.Toast;
+
+import java.util.Random;
 
 public class MyService extends Service {
 
     private final static int MODE = Service.START_NOT_STICKY;
     private MyBinder binder = new MyBinder();
+    private final Random mGenerator = new Random();
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                toast();
+                getRandomNumber();
+                stopSelf();
             }
-        });
+        }).start();
 
         return MODE;
     }
@@ -34,7 +37,7 @@ public class MyService extends Service {
         return binder;
     }
 
-    public void toast() {
-        Toast.makeText(this, "Toast from Service", Toast.LENGTH_SHORT).show();
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
     }
 }
--- app/src/main/java/com/alextroy/android/hw3_service/SecondActivity.java
+++ app/src/main/java/com/alextroy/android/hw3_service/SecondActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -62,7 +63,8 @@ public class SecondActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isConnected) {
-                    mService.toast();
+                    int num = mService.getRandomNumber();
+                    Toast.makeText(v.getContext(), "number: " + num, Toast.LENGTH_SHORT).show();
                 }
             }
         });
